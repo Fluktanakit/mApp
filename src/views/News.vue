ff3e9b153ce6217d1771007c21d1e04714d6d986
Fluktanakit <tanakit_ke62@live.rmutl.ac.th>
2022-02-07 21:13:40 +0700

    week11

--- src/views/News.vue
+++ src/views/News.vue
@@ -6,59 +6,54 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">-ข่าวสาร</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
       <ion-card>
-  <ion-item>
-    <ion-icon name="pin" slot="start"></ion-icon>
-    <ion-label>รายละเอียด</ion-label>
-    <ion-button fill="outline" slot="end">View</ion-button>
-  </ion-item>
+        <ion-item>
+          <ion-icon :icon="pin" slot="start"></ion-icon>
+          <ion-label>ข่าวสาร 1</ion-label>
+          <ion-button fill="outline" slot="end">View</ion-button>
+        </ion-item>
 
-  <ion-card-content>
-   รายละเอียด
-  </ion-card-content>
-</ion-card>
-<ion-card>
-  <ion-item>
-    <ion-icon name="pin" slot="start"></ion-icon>
-    <ion-label>รายละเอียด</ion-label>
-    <ion-button fill="outline" slot="end">View</ion-button>
-  </ion-item>
+        <ion-card-content>
+          This is content, without any paragraph or header tags,
+          within an ion-card-content element.
+        </ion-card-content>
+      </ion-card>
+      <ion-card>
+        <ion-item>
+          <ion-icon :icon="pin" slot="start"></ion-icon>
+          <ion-label>ข่าวสาร 2</ion-label>
+          <ion-button fill="outline" slot="end">View</ion-button>
+        </ion-item>
 
-  <ion-card-content>
-   รายละเอียด
-  </ion-card-content>
-</ion-card>
-<ion-card>
-  <ion-item>
-    <ion-icon name="pin" slot="start"></ion-icon>
-    <ion-label>รายละเอียด</ion-label>
-    <ion-button fill="outline" slot="end">View</ion-button>
-  </ion-item>
+        <ion-card-content>
+          This is content, without any paragraph or header tags,
+          within an ion-card-content element.
+        </ion-card-content>
+      </ion-card>
+      <ion-card>
+        <ion-item>
+          <ion-icon :icon="pin" slot="start"></ion-icon>
+          <ion-label>ข่าวสาร 3</ion-label>
+          <ion-button fill="outline" slot="end">View</ion-button>
+        </ion-item>
 
-  <ion-card-content>
-   รายละเอียด
-  </ion-card-content>
-</ion-card>
-      <ExploreContainer name="ข่าวสาร" />
+        <ion-card-content>
+          This is content, without any paragraph or header tags,
+          within an ion-card-content element.
+        </ion-card-content>
+      </ion-card>
     </ion-content>
   </ion-page>
-
-  
 </template>
 
 <script lang="ts">
-//import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-//import ExploreContainer from '../components/ExploreContainer.vue';
-
-
+import { IonCard, IonCardContent, IonIcon, IonItem, IonLabel, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/vue';
+import { pin } from 'ionicons/icons';
 export default  {
- // name: 'News',
- //  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  name: 'News',
+  components: { IonCard, IonCardContent, IonIcon, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonButton },
+  setup() {
+    return { pin };
+  }
 }
 </script>
